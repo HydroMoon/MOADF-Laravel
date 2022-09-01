e830b99c766c7478663c571f41e9488c099b35af
Qasim Nouh <vs.dev50@gmail.com>
2022-09-01 17:36:01 +0300

    [ADD] artisan commands for old laravel

--- moadf/startup/artisan.sh
+++ moadf/startup/artisan.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 # Startup script for optimizing and setting up laravel
 cd /var/app
-artisan key:generate
+export COMPOSER_HOME=/var/composer/.composer
+export COMPOSER_MEMORY_LIMIT=-1 
+# composer update
+# artisan key:generate
 artisan clear-compiled
-COMPOSER_HOME=/var/composer/.composer composer dump-autoload
+composer dump-autoload
+# artisan cache:clear
+# artisan route:clear
+# artisan config:clear
+# artisan view:clear
+artisan optimize
 artisan storage:link
-# artisan optimize
